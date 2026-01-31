04c38f534171f63791b6fbac20533b47648d1501
Alex Noppenberger <noppenbe@gmx.de>
2026-01-31 13:57:23 +0100

    Update DeployController.php

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -11,7 +11,7 @@ class DeployController extends Controller
         return view('deploy');
     }
 
-    public function deploy(Request $request)
+   public function deploy(Request $request)
     {
         $request->validate([
             'token' => 'required'
@@ -24,17 +24,17 @@ class DeployController extends Controller
         $output = [];
         $result = 0;
 
-        // Dynamischer Pfad basierend auf OS
         $basePath = base_path();
         $isWindows = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
 
         if ($isWindows) {
-            // Windows: PowerShell Script oder Git Pull
-            $command = "cd /d {$basePath} && git pull origin main 2>&1";
+            $command = "cd /d {$basePath} && git pull origin master 2>&1";
             exec($command, $output, $result);
         } else {
-            // Linux: Bash Script
-            exec("cd {$basePath} && bash deploy.sh 2>&1", $output, $result);
+            // Kompletter Pfad zum Script
+            $scriptPath = $basePath . '/deploy.sh';
+            $command = "bash {$scriptPath} 2>&1";
+            exec($command, $output, $result);
         }
 
         $message = $result === 0 ? 'Deployment erfolgreich!' : 'Deployment fehlgeschlagen!';
